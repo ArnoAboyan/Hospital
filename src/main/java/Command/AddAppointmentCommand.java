88dc7ffdaf5f9d2611698bc5ab2c2691bf232337
withkeygames <withkeygames@gmail.com>
2023-01-25 01:00:20 +0200

    v1.4 Validation

--- src/main/java/Command/AddAppointmentCommand.java
+++ src/main/java/Command/AddAppointmentCommand.java
@@ -26,44 +26,51 @@ public class AddAppointmentCommand implements Command {
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws DAOException, CommandException {
         logger.info("Execute ==> AddAppointmentCommand...");
+        Appointment appointment = new Appointment();
+
+        LocalDateTime nowDateTime = LocalDateTime.now();
 
         int doctorID = Integer.parseInt(req.getParameter("doctor"));
         int patientId = Integer.parseInt(req.getParameter("patientid"));
         String appointmentdate = (req.getParameter("appointmentdata"));
-        System.out.println(appointmentdate);
 
-
-        Appointment appointment = new Appointment();
-
-        if (doctorID > 0) {
-            appointment.setDoctorId(doctorID);
+        if (appointmentdate == null) {
+            logger.error("Problem with searching  appointmentdate " + appointmentdate);
+            throw new CommandException("Problem with date => " + appointmentdate);
         } else {
-            logger.error("Problem with searching  patient " + doctorID );
-            throw new CommandException ("Problem with searching  doctor");
+            LocalDateTime actualdate = LocalDateTime.parse(appointmentdate);
+
+
+            if (doctorID > 0) {
+                appointment.setDoctorId(doctorID);
+            } else {
+                logger.error("Problem with searching  doctor " + doctorID);
+                throw new CommandException("Problem with searching  doctor");
+            }
+
+            if (patientId > 0) {
+                appointment.setPatientId(patientId);
+            } else {
+                logger.error("Problem with searching  patient " + patientId);
+                throw new CommandException("Problem with searching  patient");
+            }
+            //DATE VERIFICATION//
+            if (!actualdate.isBefore(nowDateTime)) {
+                appointment.setAppointmentData(actualdate);
+            } else {
+                logger.error("The chosen time is already in the past " + actualdate);
+                throw new CommandException("The chosen time is already in the past " + actualdate);
+            }
+
+
+            AppointmentDao appointmentDao = new AppointmentDao();
+            appointmentDao.create(appointment);
+
+            logger.info("New appointment create => " + appointment);
+
+
+            return "controller?command=patientlistcommand&page=1";
+
         }
-
-        if (patientId > 0) {
-            appointment.setPatientId(patientId);
-        } else {
-            logger.error("Problem with searching  patient " + patientId );
-            throw new CommandException ("Problem with searching  patient");
-        }
-        if (appointmentdate != null) {
-            appointment.setAppointmentData(LocalDateTime.parse(appointmentdate));
-        } else {
-            logger.error("Problem with searching  patient " + appointmentdate );
-            throw new CommandException ("Problem with searching  appointment date");
-        }
-
-
-        AppointmentDao appointmentDao = new AppointmentDao();
-        appointmentDao.create(appointment);
-
-        logger.info("New appointment create => " + appointment);
-
-
-
-        return "controller?command=patientlistcommand&page=1";
-
     }
 }
