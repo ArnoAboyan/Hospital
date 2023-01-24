6a9fd1731a20ecb826905cb53cd4ac6d6f03e359
withkeygames <withkeygames@gmail.com>
2023-01-25 01:02:21 +0200

    v1.4 Validation

--- src/main/java/listener/ContextListener.java
+++ src/main/java/listener/ContextListener.java
@@ -1,21 +1,26 @@
 package listener;
 
+import Command.AddAppointmentCommand;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
+import org.apache.log4j.Logger;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+
+
 @WebListener
 public class ContextListener implements ServletContextListener {
 
+    static final Logger logger = Logger.getLogger(ContextListener.class);
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext ctx = sce.getServletContext();
-//        logger.info("ContextListener#contextInitialized get Servlet context = " + ctx);
+        logger.info("ContextListener#contextInitialized get Servlet context = " + ctx);
 
 
         // file name with locales descriptions
@@ -29,12 +34,12 @@ public class ContextListener implements ServletContextListener {
         try {
             locales.load(new FileInputStream(localesFileRealPath));
         } catch (IOException e) {
-//            logger.error("ContextListener#contextInitialized get error IOException = " + e);
+            logger.error("ContextListener#contextInitialized get error IOException = " + e);
         }
 
         // save descriptions to servlet context
         ctx.setAttribute("locales", locales);
 
-//        logger.debug("locales ==> " + locales);
+               logger.debug("locales ==> " + locales);
     }
 }
